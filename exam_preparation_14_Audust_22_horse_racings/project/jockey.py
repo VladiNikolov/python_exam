c92ec1d76de1c4ee3409095fe8d84e0d0bee8346
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-05-29 09:01:18 +0300

    Create jockey.py

---
+++ exam_preparation_14_Audust_22_horse_racings/project/jockey.py
@@ -0,0 +1,39 @@
+from project.core.validator import Validator
+
+
+class Jockey:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+        self.horse = None
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value.strip() == "":
+            raise ValueError("Name should contain at least one character!")
+        self.__name = value
+
+        # Validator.raise_if_name_is_empty_string_or_whitespace(value, "Name should contain at least one character!")
+        # self.__name = value
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 18:
+            raise ValueError("Jockeys must be at least 18 to participate in the race!")
+        self.__age = value
+
+        # Validator.raise_if_age_is_under_18(value, "Jockeys must be at least 18 to participate in the race!")
+        # self.__age = value
+
+
+
+
