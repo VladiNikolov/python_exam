328efd411a2e18672d9693e55439cd91c668812c
Vladimir Nikolov <vladimir.pl.nikolov@gmail.com>
2023-06-05 14:42:18 +0300

    Create private_booth.py

---
+++ exam_preparation_10_December_22_christmas_pastry_shop/project/booths/private_booth.py
@@ -0,0 +1,14 @@
+from project.booths.booth import Booth
+
+
+class PrivateBooth(Booth):
+    def __init__(self, booth_number: int, capacity: int):
+        super().__init__(booth_number, capacity)
+
+    @property
+    def get_price_per_person(self):
+        return 3.50
+
+    def reserve(self, number_of_people: int):
+        self.price_for_reservation = number_of_people * self.get_price_per_person
+        self.is_reserved = True
